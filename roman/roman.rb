455dee1134779d81ee4be9430215bc251e781075
Elli <ebolloff@tzi.de>
2014-03-03 00:22:46 +0100

    test for roman and roman

--- roman/roman.rb
+++ roman/roman.rb
@@ -1,3 +1,69 @@
-class RomanNumber
+class RomanNumber 
+ 
+  @@Symbols = { 1=>'I', 5=>'V', 10=>'X', 50=>'L', 100=>'C', 500=>'D', 1000=>'N' }
+  @@Subtractors = [ [1000, 100], [500, 100], [100, 10], [50, 10], [10, 1], [5, 1], [1, 0] ]
 
+  def initialize(no)
+    @number = no
+  end
+
+  private
+  def roman(num)
+    #Thanks to http://rosettacode.org/wiki/Roman_numerals/Encode#Ruby
+    return @@Symbols[num] if @@Symbols.has_key?(num)
+    @@Subtractors.each do |cutPoint, subtractor|
+      return roman(cutPoint) + roman(num - cutPoint)      if num > cutPoint
+      return roman(subtractor) + roman(num + subtractor)  if num >= cutPoint - subtractor and num < cutPoint
+    end
+    return ""
+  end
+
+  public
+  def to_s
+    roman(@number)
+  end
+
+  def to_i
+    @number
+  end
+
+  def +(no)
+    RomanNumber.new (@number + no.to_i)
+  end
+
+  def -(no)
+    RomanNumber.new (@number - no.to_i)
+  end
+
+  def *(no)
+    RomanNumber.new (@number * no.to_i)
+  end
+
+  def /(no)
+    RomanNumber.new (@number / no.to_i)
+  end
+
+  def ==(no)
+    @number == no.to_i
+  end
+
+  def !=(no)
+    not @number == no.to_i
+  end
+
+  def >(no)
+    @number > no.to_i
+  end
+
+  def <(no)
+    @number < no.to_i
+  end
+
+  def >=(no)
+   self > no || self == no
+  end
+
+  def <=(no)
+   self < no || self == no
+  end
 end
--- roman/roman_spec.rb
+++ roman/roman_spec.rb
@@ -13,6 +13,74 @@ describe RomanNumber do
 
   it "should return the string representation of the roman number 0" do
     no = RomanNumber.new(0)
-    no.to_s.should == "N"
+    no.to_s.should == ""
+  end
+
+  it "should convert roman number to int" do
+    RomanNumber.new(15).to_i.should == 15
+  end
+  
+  it "should add two numbers" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(23)
+    (x + y).should == 28
+  end
+
+  it "should subtract two numbers" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(23)
+    (y - x).should == 18
+  end
+
+  it "should multiply two numbers" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(23)
+    (x * y).should == 115
+  end
+
+  it "should divide two numbers" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(20)
+    (y / x).should == 4
+  end
+
+  it "should check if two numbers are equal" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(5)
+    (x == y).should == true
+  end
+
+  it "should check if two numbers are unequal" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(23)
+    (x != y).should == true
+  end
+
+  it "should check if one number is bigger than the other" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(23)
+    (y > x).should == true
+  end
+
+  it "should check if one number is smaller than the other" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(23)
+    (x < y).should == true
+  end
+
+  it "should check if one number is bigger or equal than the other" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(5)
+    z = RomanNumber.new(6)
+    (x >= y).should == true
+    (z >= x).should == true
+  end
+
+  it "should check if one number is smaller or equal than the other" do
+    x = RomanNumber.new(5)
+    y = RomanNumber.new(5)
+    z = RomanNumber.new(4)
+    (x <= y).should == true
+    (z <= x).should == true
   end
 end

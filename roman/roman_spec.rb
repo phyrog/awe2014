b18a08b94953825f47657a9bcf3cc38e3c0e2539
Tom Gehrke <tgehrke@tzi.de>
2014-03-03 20:09:01 +0100

    Added testcase for integer to RomanNumber conversion and refactored other testcases to use to_r

--- roman/roman_spec.rb
+++ roman/roman_spec.rb
@@ -1,85 +1,90 @@
 require_relative 'roman'
 
 describe RomanNumber do
+
+  it "should convert an integer to a roman number" do
+    10.to_r.should == RomanNumber.new(10)
+  end
+
   it "should return the string representation of the roman number 15" do
-    no = RomanNumber.new(15)
+    no = 15.to_r
     no.to_s.should == "XV"
   end
 
   it "should return the string representation of the roman number 4" do
-    no = RomanNumber.new(4)
+    no = 4.to_r
     no.to_s.should == "IV"
   end
 
   it "should return the string representation of the roman number 0" do
-    no = RomanNumber.new(0)
+    no = 0.to_r
     no.to_s.should == ""
   end
 
   it "should convert roman number to int" do
-    RomanNumber.new(15).to_i.should == 15
+    15.to_r.to_i.should == 15
   end
   
   it "should add two numbers" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(23)
-    (x + y).should == 28
+    x = 5.to_r
+    y = 23.to_r
+    (x + y).should == 28.to_r
   end
 
   it "should subtract two numbers" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(23)
+    x = 5.to_r
+    y = 23.to_r
     (y - x).should == 18
   end
 
   it "should multiply two numbers" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(23)
+    x = 5.to_r
+    y = 23.to_r
     (x * y).should == 115
   end
 
   it "should divide two numbers" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(20)
+    x = 5.to_r
+    y = 20.to_r
     (y / x).should == 4
   end
 
   it "should check if two numbers are equal" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(5)
+    x = 5.to_r
+    y = 5.to_r
     x.should == y
   end
 
   it "should check if two numbers are unequal" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(23)
+    x = 5.to_r
+    y = 23.to_r
     x.should_not == y
   end
 
   it "should check if one number is bigger than the other" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(23)
+    x = 5.to_r
+    y = 23.to_r
     y.should > x
   end
 
   it "should check if one number is smaller than the other" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(23)
+    x = 5.to_r
+    y = 23.to_r
     x.should < y
   end
 
   it "should check if one number is bigger or equal than the other" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(5)
-    z = RomanNumber.new(6)
+    x = 5.to_r
+    y = 5.to_r
+    z = 6.to_r
     x.should >= y
     z.should >= x
   end
 
   it "should check if one number is smaller or equal than the other" do
-    x = RomanNumber.new(5)
-    y = RomanNumber.new(5)
-    z = RomanNumber.new(4)
+    x = 5.to_r
+    y = 5.to_r
+    z = 4.to_r
     x.should <= y
     z.should <= x
   end

b82640d1907b2dd6e591e0afe7ab807947c30117
Tom Gehrke <tgehrke@tzi.de>
2014-03-03 20:25:43 +0100

    Added code to convert a string to a roman number

--- roman/roman.rb
+++ roman/roman.rb
@@ -20,7 +20,7 @@ class RomanNumber
 
   public
   def to_s
-    roman(@number)
+    roman(@number) if @number >= 0
   end
 
   def to_i
@@ -71,5 +71,41 @@ end
 class Integer
   def to_r
     RomanNumber.new(self)
+  end
+end
+
+class String
+  private
+  def fromRoman(roman)
+    # Thanks to http://rosettacode.org/wiki/Roman_numerals/Decode#Ruby
+    r = roman.upcase
+    n = 0
+    until r.empty? do
+      case
+      when r.start_with?('M')  then v = 1000; len = 1
+      when r.start_with?('CM') then v = 900;  len = 2
+      when r.start_with?('D')  then v = 500;  len = 1
+      when r.start_with?('CD') then v = 400;  len = 2
+      when r.start_with?('C')  then v = 100;  len = 1
+      when r.start_with?('XC') then v = 90;   len = 2
+      when r.start_with?('L')  then v = 50;   len = 1
+      when r.start_with?('XL') then v = 40;   len = 2
+      when r.start_with?('X')  then v = 10;   len = 1
+      when r.start_with?('IX') then v = 9;    len = 2
+      when r.start_with?('V')  then v = 5;    len = 1
+      when r.start_with?('IV') then v = 4;    len = 2
+      when r.start_with?('I')  then v = 1;    len = 1
+      else
+        raise ArgumentError.new("invalid roman numerals: " + roman)
+      end
+      n += v
+      r.slice!(0,len)
+    end
+    n
+  end
+  
+  public
+  def to_r
+    fromRoman(self).to_r
   end
 end

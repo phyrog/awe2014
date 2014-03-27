1ef84b734864453e0189f708f19c579a637b529d
Elli <ebolloff@tzi.de>
2014-03-27 17:41:06 +0100

    done parts of chapter 4

--- chapter3/sample_app/spec/requests/static_pages_spec.rb
+++ chapter3/sample_app/spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
     
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -22,9 +27,14 @@ describe "StaticPages" do
       expect(page).to have_content('Help')
     end
   
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title('| Help')
     end
   end
 
@@ -35,10 +45,14 @@ describe "StaticPages" do
       expect(page).to have_content('About Us')
     end
     
-    it "should have the title 'About Us'" do 
+    it "should have the base title" do 
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      expect(page).not_to have_title('| About Us')
     end
   end
 end
-
